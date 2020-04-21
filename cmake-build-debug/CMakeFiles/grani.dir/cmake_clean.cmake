327b2e127dcd3bb610ed665d000e5572f5578daa
Софья Андреева <skandreeva@edu.hse.ru>
2020-04-21 14:44:48 +0300

    fix

---
+++ cmake-build-debug/CMakeFiles/grani.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/grani.dir/main.cpp.obj"
+  "grani.pdb"
+  "grani.exe"
+  "grani.exe.manifest"
+  "libgrani.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/grani.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
